2c214eaa2983a917ab1f8707800fa03936f947ca
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2023-02-21 12:54:12 +0200

    Create Albums comopnent

---
+++ components/Albums/index.tsx
@@ -0,0 +1,33 @@
+import { AlbumEntity } from '@app/types/album';
+import React from 'react';
+import { FlatList } from 'react-native';
+import Album from '../Album';
+
+import styles from './styles';
+
+type AlbumsProps = {
+  albums: AlbumEntity[];
+  handlePlayAlbum: (albumId: string) => void;
+  handleRemoveAlbum: (albumId: string) => void;
+};
+
+const Albums: React.FC<AlbumsProps> = ({ albums, handlePlayAlbum, handleRemoveAlbum }) => {
+  const renderItem = ({ item }: { item: AlbumEntity }) => {
+    return (
+      <Album data={item} handlePlayAlbum={handlePlayAlbum} handleRemoveAlbum={handleRemoveAlbum} />
+    );
+  };
+
+  return (
+    <FlatList
+      data={albums}
+      keyExtractor={(item) => item.id}
+      renderItem={renderItem}
+      style={{ width: '100%' }}
+      numColumns={2} // Add this prop to make it display items in two columns
+      columnWrapperStyle={styles.columnWrapper} // Add this to style the items in the grid
+    />
+  );
+};
+
+export default Albums;
---
+++ components/Albums/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  columnWrapper: {
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+});
