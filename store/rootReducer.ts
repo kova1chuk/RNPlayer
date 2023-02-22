87fccfcae480a85971245026fe397f581bf5f290
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2023-02-22 14:35:07 +0200

    Clear unused sore code

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
 import albumsReducer from './albums/reducer';
-
-import songsReducer from './songs/reducer';
 import tracksReducer from './tracks/reducer';
 
 const rootReducer = combineReducers({
-  songs: songsReducer,
   albums: albumsReducer,
   tracks: tracksReducer,
 });
--- store/rootSaga.ts
+++ store/rootSaga.ts
@@ -1,7 +1,5 @@
 import { all, fork } from 'redux-saga/effects';
 
-import songsSaga from './songs/sagas';
-
 export function* rootSaga() {
   // yield all([fork(songsSaga)]);
 }
